35d9ea4c4a5532076f598cac2b9d4367e4f92762
Yifei Zhan <yifei.zhan@c02zj0xqlvdt.check24.intern>
2019-11-11 17:55:50 +0100

    some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './theme.less';
+import { InsuranceList } from './components/insurance-list/insurance-list';
+import SideBar from './components/sidebar/sidebar';
+import styles from './index.module.less';
+import Header from './components/header-group/header-group';
 
 ReactDOM.render(
-  <div>Hello World</div>,
+  <main>
+    <header className={styles['header']}>
+      <Header/>
+    </header>
+    <main className={styles['content']}>
+      <SideBar />
+      <InsuranceList />
+    </main>
+  </main>
+  ,
   document.getElementById('root')
 );
