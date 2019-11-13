c54b9547504960ad6c8e0793dad1e47ca73c0952
Yifei Zhan <yifei.zhan@c02zj0xqlvdt.check24.intern>
2019-11-13 09:26:28 +0100

    add more changes

--- src/components/header-group/header-group.tsx
+++ src/components/header-group/header-group.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './header-group.module.less'
 
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className={styles['wrapper']}>
+      <div className={styles['title']}>Welcome to the world of CSSModules</div>
+      <div className="logo">CHECK24</div>
     </div>
   )
 }
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,7 +2,23 @@ import React from 'react';
 import styles from './sidebar.module.less';
 
 export default function SideBar() {
+  const date = new Date();
+
   return (
-    <div className={styles['wrapper']}>sidebar</div>
+    <div className={styles['wrapper']}>
+      <header className={styles['header']}>Sidebar Title</header>
+      <div className="logo">CHECK24</div>
+      <ul>
+        <li>Item A</li>
+        <li>Item B</li>
+        <li>Item C</li>
+      </ul>
+      <div className={styles['footer']}>
+        <div>@CHECK24</div>
+        <div className={styles['timer']}>
+          {date.getFullYear()}-{date.getMonth()}
+        </div>
+      </div>
+    </div>
   )
 }
